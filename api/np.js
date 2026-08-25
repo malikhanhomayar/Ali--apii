842818893ef7cc7ee971d5217ee6caa36500403e
malikhanhomayar <newfreshtx@gmail.com>
2026-08-25 14:19:35 +0500

    np.js

--- api/np.js
+++ api/np.js
@@ -6,7 +6,7 @@ const { CookieJar } = require('tough-cookie');
 const router = express.Router();
 
 // --- Configuration ---
-const baseURL = "https://tempnumbers.net/NumberPanel";
+const baseURL = "https://tempnumbers.net";
 const username = "Rahman007";
 const password = "Rahman007";
 const userAgent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Mobile Safari/537.36";
